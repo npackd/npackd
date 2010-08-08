0732917dea5e01d11d7c28d3d030eb740e64a6cc
tim lebedkov <tim.lebedkov@googlemail.com>
2010-08-08 11:03:37 +0200

    1.12.0

--- wpmcpp/ui_settingsdialog.h
+++ wpmcpp/ui_settingsdialog.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'settingsdialog.ui'
 **
-** Created: Sat 7. Aug 11:50:32 2010
+** Created: Sat 7. Aug 22:13:09 2010
 **      by: Qt User Interface Compiler version 4.6.3
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
