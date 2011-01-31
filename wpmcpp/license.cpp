c26f699f8c4cb0685a737b397f28926fff713f8e
tim lebedkov <tim.lebedkov@googlemail.com>
2011-01-31 18:10:40 +0100

    License class

---
+++ wpmcpp/license.cpp
@@ -0,0 +1,5 @@
+#include "license.h"
+
+License::License()
+{
+}
---
+++ wpmcpp/license.h
@@ -0,0 +1,23 @@
+#ifndef LICENSE_H
+#define LICENSE_H
+
+#include "qstring.h"
+
+/**
+ * License description.
+ */
+class License
+{
+public:
+    QString id;
+
+    QString title;
+
+    QString description;
+
+    QString url;
+
+    License();
+};
+
+#endif // LICENSE_H
