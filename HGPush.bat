43cdaacc78d82bcafff0305d8b851d1b5f6e87e7
tim lebedkov <tim.lebedkov@googlemail.com>
2011-02-13 11:28:58 +0100

    PDF-XChange Viewer/64 bit 2.5.192

--- HGPush.bat
+++ HGPush.bat
@@ -1,6 +1,9 @@
 C:\WPM\org.xmlsoft.LibXML-2.4.12\bin\xmllint.exe --noout repository\Rep.xml
 if %errorlevel% neq 0 pause & goto eof
 
+C:\WPM\org.xmlsoft.LibXML-2.4.12\bin\xmllint.exe --noout repository\Rep64.xml
+if %errorlevel% neq 0 pause & goto eof
+
 call hg push
 
 pause
