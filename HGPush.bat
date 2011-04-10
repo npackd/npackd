d14d907a80ff5b3efc68892ae95d3f34c74f2ae2
tim lebedkov <tim.lebedkov@googlemail.com>
2011-04-10 15:11:48 +0200

    find the newest Hg

--- HGPush.bat
+++ HGPush.bat
@@ -1,9 +1,15 @@
-C:\WPM\LibXML-2.4.12\bin\xmllint.exe --noout repository\Rep.xml
+set onecmd="%npackd_cl%\npackdcl.exe" path --package=org.xmlsoft.LibXML --versions=[2.4.12,2.4.12]
+for /f "usebackq delims=" %%x in (`%%onecmd%%`) do set libxml=%%x
+
+set onecmd="%npackd_cl%\npackdcl.exe" path --package=com.selenic.mercurial.Mercurial64 --versions=[1.8,2)
+for /f "usebackq delims=" %%x in (`%%onecmd%%`) do set mercurial=%%x
+
+"%libxml%\bin\xmllint.exe" --noout repository\Rep.xml
 if %errorlevel% neq 0 pause & goto eof
 
-C:\WPM\LibXML-2.4.12\bin\xmllint.exe --noout repository\Rep64.xml
+"%libxml%\bin\xmllint.exe" --noout repository\Rep64.xml
 if %errorlevel% neq 0 pause & goto eof
 
-call hg push
+"%mercurial%\hg.exe" push
 
 pause
