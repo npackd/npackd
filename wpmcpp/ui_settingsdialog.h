a744e5dac57b9f1c5df819fbe6b882516cb9b004
tim lebedkov <tim.lebedkov@googlemail.com>
2011-06-12 16:35:09 +0200

    tab order in the settings dialog

--- wpmcpp/ui_settingsdialog.h
+++ wpmcpp/ui_settingsdialog.h
@@ -1,8 +1,8 @@
 /********************************************************************************
 ** Form generated from reading UI file 'settingsdialog.ui'
 **
-** Created: Thu 7. Apr 21:02:52 2011
-**      by: Qt User Interface Compiler version 4.6.3
+** Created: Sun 12. Jun 13:50:39 2011
+**      by: Qt User Interface Compiler version 4.7.3
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
 ********************************************************************************/
@@ -96,6 +96,8 @@ public:
 #ifndef QT_NO_SHORTCUT
         label_2->setBuddy(lineEditDir);
 #endif // QT_NO_SHORTCUT
+        QWidget::setTabOrder(plainTextEditReps, lineEditDir);
+        QWidget::setTabOrder(lineEditDir, buttonBox);
 
         retranslateUi(SettingsDialog);
         QObject::connect(buttonBox, SIGNAL(accepted()), SettingsDialog, SLOT(accept()));
