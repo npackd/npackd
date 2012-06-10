1e065f950a2d7f2a3da32e3a34de92f92b52dc81
tim lebedkov <tim.lebedkov@googlemail.com>
2012-06-10 19:56:59 +0200

    use Npackd installer helper 1.1

--- installer-helper/_UninstallMSISimple.bat
+++
@@ -1,4 +0,0 @@
-if "%npackd_cl%" equ "" set npackd_cl=..\com.googlecode.windows-package-manager.NpackdCL-1
-set onecmd="%npackd_cl%\npackdcl.exe" "path" "--package=com.googlecode.windows-package-manager.NpackdInstallerHelper" "--versions=[1, 1]"
-for /f "usebackq delims=" %%x in (`%%onecmd%%`) do set npackdih=%%x
-call "%npackdih%\UninstallMSI.bat"
