7b5eb27a9cdddb8555b14bbdac47643863ca493f
tim lebedkov <tim.lebedkov@googlemail.com>
2012-07-01 09:44:01 +0200

    updated HGPush.bat

--- HGPush.bat
+++ HGPush.bat
@@ -1,23 +1,19 @@
 set onecmd="%npackd_cl%\npackdcl.exe" path --package=org.xmlsoft.LibXML --versions=[2.4.12,2.4.12]
 for /f "usebackq delims=" %%x in (`%%onecmd%%`) do set libxml=%%x
 
-set onecmd="%npackd_cl%\npackdcl.exe" path --package=com.selenic.mercurial.Mercurial64 --versions=[1.8,2)
+set onecmd="%npackd_cl%\npackdcl.exe" path --package=com.selenic.mercurial.Mercurial64 --versions=[1.8,3)
 for /f "usebackq delims=" %%x in (`%%onecmd%%`) do set mercurial=%%x
 
-set onecmd="%npackd_cl%\npackdcl.exe" path --package=net.sourceforge.gnuwin32.CoreUtils --versions=[5.3,5.3]
-for /f "usebackq delims=" %%x in (`%%onecmd%%`) do set coreutils=%%x
-
 "%libxml%\bin\xmllint.exe" --noout repository\Rep.xml
-if %errorlevel% neq 0 pause & goto eof
+if %errorlevel% neq 0 pause & goto :eof
 
 "%libxml%\bin\xmllint.exe" --noout repository\Rep64.xml
-if %errorlevel% neq 0 pause & goto eof
+if %errorlevel% neq 0 pause & goto :eof
 
 "%libxml%\bin\xmllint.exe" --noout repository\Libs.xml
-if %errorlevel% neq 0 pause & goto eof
+if %errorlevel% neq 0 pause & goto :eof
 
 "%mercurial%\hg.exe" push
-if %errorlevel% neq 0 pause & goto eof
+if %errorlevel% neq 0 pause & goto :eof
 
 @echo ============================ SUCCESS ======================================
-"%coreutils%\bin\sleep" 10
