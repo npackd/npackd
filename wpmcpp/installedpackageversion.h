9d0ebc426002446d012b79801b5d90ea5011bf7b
tim lebedkov <tim.lebedkov@googlemail.com>
2013-02-09 12:36:46 +0100

    move package detection code to InstalledPackages

--- wpmcpp/installedpackageversion.h
+++ wpmcpp/installedpackageversion.h
@@ -3,19 +3,52 @@
 
 #include <QString>
 
+#include "version.h"
+
 /**
  * @brief information about one installed package version
  */
 class InstalledPackageVersion
 {
-public:
-    /** program location on the disk */
+    friend class InstalledPackages;
+
     QString directory;
 
+    InstalledPackageVersion(const QString& package,
+            const Version& version,
+            const QString& directory);
+
     /**
-     * @brief -
+     * Loads the information about this package from the Windows registry.
      */
-    InstalledPackageVersion();
+    void loadFromRegistry();
+public:
+    /**
+     * The value is not empty for externally installed and detected packages.
+     * For example, packages detected from the MSI database have here the MSI
+     * product GUID.
+     *
+     * MSI: "msi:{86ce85e6-dbac-3ffd-b977-e4b79f83c909}"
+     * Contol Panel Programs: "control-panel:Active Script 1.0"
+     */
+    QString detectionInfo;
+
+    /** full package version. This value should not be chaged. */
+    QString package;
+
+    /** version number. This value should not be changed. */
+    Version version;
+
+    /**
+     * @return installation directory
+     */
+    QString getDirectory() const;
+
+    /**
+     * @brief saves the information in the Windows registry
+     * @return error message
+     */
+    QString save() const;
 };
 
 #endif // INSTALLEDPACKAGEVERSION_H
