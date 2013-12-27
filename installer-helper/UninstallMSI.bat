419ee513fd867e1646d7a14144ef05e9bfc8adbc
Tim Lebedkov <tim.lebedkov@gmail.com>
2013-12-27 14:29:22 +0100

    do not fail if an MSI product is already uninstalled

--- installer-helper/UninstallMSI.bat
+++ installer-helper/UninstallMSI.bat
@@ -7,6 +7,12 @@ rem MSIFASTINSTALL: http://msdn.microsoft.com/en-us/library/dd408005%28v=VS.85%2
 msiexec.exe /qn /norestart /Lime .Npackd\UninstallMSI.log /x "%setup%" MSIFASTINSTALL=7
 set err=%errorlevel%
 type .Npackd\UninstallMSI.log
+
 rem 3010=restart required
 if %err% equ 3010 exit 0
+
+rem 1605=unknown product
+if %err% equ 1605 exit 0
+
 if %err% neq 0 exit %err%
+
