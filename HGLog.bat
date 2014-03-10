d89f0ad3100092b468705f2efaf9cfa4341edbbf
Tim Lebedkov <tim.lebedkov@gmail.com>
2014-03-10 23:18:30 +0100

    move to npackd-scripts

--- HGLog.bat
+++
@@ -1,4 +0,0 @@
-rem this script creates a list of changes that can be used to create a
-rem what's new page like this one:
-rem http://code.google.com/p/windows-package-manager/wiki/WhatIsNewInNpackd1_16
-hg log -r 2497:default --template "  * {desc}\n" | clip
