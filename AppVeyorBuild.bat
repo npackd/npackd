e04d549f9a367a513cd712f375b20fcc74e6fa06
Tim Lebedkov <tim.lebedkov@gmail.com>
2015-10-24 11:49:29 +0200

    add appveyor path

--- AppVeyorBuild.bat
+++ AppVeyorBuild.bat
@@ -2,6 +2,6 @@ echo off
 
 start AppVeyorScreenshots.bat
 
-set path=%SystemRoot%;%SystemRoot%\system32
+set path=%SystemRoot%;%SystemRoot%\system32;C:\Program Files\AppVeyor\BuildAgent\
 @cscript TestUnstableRep.js /password:%password%
 
