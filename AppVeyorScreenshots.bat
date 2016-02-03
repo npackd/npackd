37a605e8cfe8d0168361f53ca387e30b0ea91370
tim-lebedkov <tim.lebedkov@gmail.com>
2016-02-03 19:09:43 +0100

    more screenshots

--- AppVeyorScreenshots.bat
+++ AppVeyorScreenshots.bat
@@ -5,6 +5,6 @@ set n=5
 "C:\Program Files (x86)\NirCmd_64_bit\nircmdc.exe" cmdwait 300000 savescreenshot screen%n%.png
 appveyor PushArtifact screen%n%.png || exit /b %errorlevel%
 set /a "n = n + 5"
-if %n% equ 35 goto :eof
+if %n% equ 55 goto :eof
 goto l
 
