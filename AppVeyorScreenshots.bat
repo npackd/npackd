ad079a685ff69bea3d0cd570cad993d67d9ea8dd
tim-lebedkov <tim.lebedkov@gmail.com>
2016-02-07 20:12:29 +0100

    do not check for the error code

--- AppVeyorScreenshots.bat
+++ AppVeyorScreenshots.bat
@@ -3,7 +3,6 @@ echo on
 set n=5
 :l
 "C:\Program Files (x86)\NirCmd_64_bit\nircmdc.exe" cmdwait 300000 savescreenshot screen%n%.png
-if %errorlevel% neq 0 exit /b %errorlevel%
 appveyor PushArtifact screen%n%.png
 set /a "n = n + 5"
 if %n% equ 55 goto :eof
