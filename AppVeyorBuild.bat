62379fee1d85b3b4a83ff4872fb5aeb8a5711726
tim-lebedkov <tim.lebedkov@gmail.com>
2016-02-14 22:04:07 +0100

    set npackd_cl

--- AppVeyorBuild.bat
+++ AppVeyorBuild.bat
@@ -2,6 +2,7 @@ echo off
 
 start AppVeyorScreenshots.bat
 
+SET NPACKD_CL=C:\Program Files (x86)\NpackdCL
 set path=%SystemRoot%;%SystemRoot%\system32;C:\Program Files\AppVeyor\BuildAgent\
 @cscript TestUnstableRep.js /password:%password%
 
