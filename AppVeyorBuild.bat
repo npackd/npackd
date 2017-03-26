c1e93bfeb8e19d86ff42d6a076cb4a6d7e47925c
tim-lebedkov <tim.lebedkov@gmail.com>
2017-03-26 18:50:25 +0200

    Do not create screenshots

--- AppVeyorBuild.bat
+++ AppVeyorBuild.bat
@@ -1,6 +1,6 @@
 echo on
 
-start AppVeyorScreenshots.bat
+rem start AppVeyorScreenshots.bat
 
 SET NPACKD_CL=C:\Program Files (x86)\NpackdCL
 set path=%SystemRoot%;%SystemRoot%\system32;C:\Program Files\AppVeyor\BuildAgent\
