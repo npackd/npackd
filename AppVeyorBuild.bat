b4aa4b2a5cc78a2a0a7b6725556c678b53372c67
tim-lebedkov <tim.lebedkov@gmail.com>
2019-08-03 22:43:25 +0200

    Test Go code

--- AppVeyorBuild.bat
+++ AppVeyorBuild.bat
@@ -2,6 +2,8 @@ echo on
 
 start "Screenshots" cmd.exe /c AppVeyorScreenshots.bat
 
+go run TestUnstableRep.go
+
 SET NPACKD_CL=C:\Program Files (x86)\NpackdCL
 set path=%SystemRoot%;%SystemRoot%\system32;C:\Program Files\AppVeyor\BuildAgent\
 @cscript TestUnstableRep.js /password:%password%
