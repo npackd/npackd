1547b3d086a1a58c5a48513e14220da1ea431fa4
tim-lebedkov <tim.lebedkov@gmail.com>
2019-11-22 13:15:46 -0800

    Bugfix: cannot find "go"

--- AppVeyorBuild.bat
+++ AppVeyorBuild.bat
@@ -2,6 +2,6 @@ echo on
 
 start "Screenshots" cmd.exe /c AppVeyorScreenshots.bat
 
-set path=%SystemRoot%;%SystemRoot%\system32;C:\Program Files\AppVeyor\BuildAgent\
+rem set path=%SystemRoot%;%SystemRoot%\system32;C:\Program Files\AppVeyor\BuildAgent\
 go run TestUnstableRep.go
 
