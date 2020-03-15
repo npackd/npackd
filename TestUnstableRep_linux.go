37abffff86ea016bf4f32b0396d657ba804f63b7
tim-lebedkov <tim.lebedkov@gmail.com>
2020-03-15 19:59:54 +0100

    modify the script to also download all binaries

---
+++ TestUnstableRep_linux.go
@@ -0,0 +1,61 @@
+// +build linux
+package main
+
+import (
+	"os/exec"
+	"syscall"
+	"fmt"
+	"strings"
+)
+
+// Executes the specified command, prints its output on the default output.
+//
+// dir: current directory or ""
+// cmd: this command should be executed
+// showParameters: true = output the parameters,
+// 	false = hide the parameters (e.g. for passwords)
+// return: [exit code, [output line 1, output line2, ...]]
+func exec2(dir string, program string, params string,
+	showParameters bool) (exitCode int, output []string) {
+	if showParameters {
+		fmt.Println("\"" + program + "\" " + params)
+	} else {
+		fmt.Println("\"" + program + "\" <<<parameters hidden>>>")
+	}
+
+	cmd := exec.Command(program)
+	cmd.SysProcAttr = &syscall.SysProcAttr{}
+	// Windows only cmd.SysProcAttr.CmdLine = " " + params
+	cmd.Dir = dir
+
+	out, err := cmd.CombinedOutput()
+
+	lines := strings.Split(string(out), "\r\n")
+	for _, line := range lines {
+		fmt.Println(line)
+	}
+
+	if err != nil {
+        // try to get the exit code
+        if exitError, ok := err.(*exec.ExitError); ok {
+            ws := exitError.Sys().(syscall.WaitStatus)
+            exitCode = ws.ExitStatus()
+        } else {
+            // This will happen (in OSX) if `name` is not available in $PATH,
+            // in this situation, exit code could not be get, and stderr will be
+            // empty string very likely, so we use the default fail code, and format err
+            // to string and set to stderr
+            fmt.Printf("Could not get exit code for failed program: %v, %v", program, params)
+            exitCode = 1
+        }
+    } else {
+        // success, exitCode should be 0 if go is ok
+        ws := cmd.ProcessState.Sys().(syscall.WaitStatus)
+        exitCode = ws.ExitStatus()
+    }
+
+	fmt.Printf("Exit code: %d\n", exitCode)
+    
+	return exitCode, lines
+}
+
---
+++ TestUnstableRep_windows.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+	"strings"
+)
+
+// Executes the specified command, prints its output on the default output.
+//
+// dir: current directory or ""
+// cmd: this command should be executed
+// showParameters: true = output the parameters,
+// 	false = hide the parameters (e.g. for passwords)
+// return: [exit code, [output line 1, output line2, ...]]
+func exec2(dir string, program string, params string,
+	showParameters bool) (exitCode int, output []string) {
+	if showParameters {
+		fmt.Println("\"" + program + "\" " + params)
+	} else {
+		fmt.Println("\"" + program + "\" <<<parameters hidden>>>")
+	}
+
+	cmd := exec.Command(program)
+	cmd.SysProcAttr = &syscall.SysProcAttr{}
+	cmd.SysProcAttr.CmdLine = " " + params
+	cmd.Dir = dir
+
+	out, err := cmd.CombinedOutput()
+
+	lines := strings.Split(string(out), "\r\n")
+	for _, line := range lines {
+		fmt.Println(line)
+	}
+
+	if err != nil {
+		if exitError, ok := err.(*exec.ExitError); ok {
+			fmt.Printf("Exit code: %d\n", exitError.ExitCode())
+			return exitError.ExitCode(), nil
+		}
+	}
+
+	return 0, lines
+}
+
