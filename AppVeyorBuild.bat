d9a1494a50e6f479d802ef919774706e52adf0d3
tim-lebedkov <tim.lebedkov@gmail.com>
2020-10-22 20:50:35 +0200

    Bugfix: screenshots are only necessary while testing packages

--- AppVeyorBuild.bat
+++ AppVeyorBuild.bat
@@ -1,6 +1,6 @@
 echo on
 
-start "Screenshots" cmd.exe /c AppVeyorScreenshots.bat
+if [%what%]==[test-packages] start "Screenshots" cmd.exe /c AppVeyorScreenshots.bat
 
 rem set path=%SystemRoot%;%SystemRoot%\system32;C:\Program Files\AppVeyor\BuildAgent\
 go run TestUnstableRep.go TestUnstableRep_windows.go -command %what%
