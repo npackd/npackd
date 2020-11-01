799100c0834bbcab9630e7340c742f9da90a623f
tim-lebedkov <tim.lebedkov@gmail.com>
2020-11-01 12:36:12 +0100

    fail while uploading a file

--- TestUnstableRep.go
+++ TestUnstableRep.go
@@ -20,6 +20,9 @@ import (
 	"strings"
 	"time"
 	"crypto/sha256"
+	"mime/multipart"
+	"sync"	
+	"bufio"
 )
 
 var changeData = false
@@ -214,7 +217,7 @@ func uploadToGithub(from string, fullPackage string,
 	// fmt.Println("Uploading to " + url);
 	fmt.Println("Download from " + downloadURL)
 
-	result, _ = exec2("", settings.curl, "-f -H \"Authorization: token "+settings.githubToken+"\""+
+	result, _ = exec2("", settings.curl, "-H \"Authorization: token "+settings.githubToken+"\""+
 		" -H \"Content-Type: "+mime+"\""+
 		" --data-binary @"+file+" \""+url+"\"", false)
 	if result != 0 {
@@ -792,6 +795,58 @@ func download(address string, showParameters bool) (*bytes.Buffer, int, error) {
 	}
 
 	return b, resp.StatusCode, nil
+}
+
+func uploadFileMultipart(url string, path string) (*http.Response, error) {
+	f, err := os.OpenFile(path, os.O_RDONLY, 0644)
+	if err != nil {
+		return nil, err
+	}
+
+	// Reduce number of syscalls when reading from disk.
+	bufferedFileReader := bufio.NewReader(f)
+	defer f.Close()
+
+	// Create a pipe for writing from the file and reading to
+	// the request concurrently.
+	bodyReader, bodyWriter := io.Pipe()
+	formWriter := multipart.NewWriter(bodyWriter)
+
+	// Store the first write error in writeErr.
+	var (
+		writeErr error
+		errOnce  sync.Once
+	)
+	setErr := func(err error) {
+		if err != nil {
+			errOnce.Do(func() { writeErr = err })
+		}
+	}
+	go func() {
+		partWriter, err := formWriter.CreateFormFile("file", path)
+		setErr(err)
+		_, err = io.Copy(partWriter, bufferedFileReader)
+		setErr(err)
+		setErr(formWriter.Close())
+		setErr(bodyWriter.Close())
+	}()
+
+	req, err := http.NewRequest(http.MethodPut, url, bodyReader)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("Content-Type", formWriter.FormDataContentType())
+
+	// This operation will block until both the formWriter
+	// and bodyWriter have been closed by the goroutine,
+	// or in the event of a HTTP error.
+	resp, err := http.DefaultClient.Do(req)
+
+	if writeErr != nil {
+		return nil, writeErr
+	}
+
+	return resp, err
 }
 
 // Download to a file
