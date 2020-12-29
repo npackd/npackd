800dba88f0419626637a2089a5c5b3a7282f0c4d
tim-lebedkov <tim.lebedkov@gmail.com>
2020-12-29 18:44:23 +0100

    remove NirCMD

--- AppVeyorBuild.bat
+++ AppVeyorBuild.bat
@@ -1,7 +1,5 @@
 echo on
 
-rem if [%what%]==[test-packages] start "Screenshots" cmd.exe /c AppVeyorScreenshots.bat
-
 rem set path=%SystemRoot%;%SystemRoot%\system32;C:\Program Files\AppVeyor\BuildAgent\
 go run TestUnstableRep.go TestUnstableRep_windows.go -command %what%
 
--- AppVeyorInstall.bat
+++ AppVeyorInstall.bat
@@ -5,8 +5,6 @@ rem software dependencies.
 
 msiexec.exe /qn /i https://github.com/tim-lebedkov/npackd-cpp/releases/download/version_1.25/NpackdCL64-1.25.0.msi
 
-rem tree "C:\Program Files"
-
 SET NPACKD_CL=C:\Program Files\NpackdCL
 "%npackd_cl%\ncl" set-repo -u https://npackd.appspot.com/rep/recent-xml -u https://npackd.appspot.com/rep/xml?tag=stable -u https://npackd.appspot.com/rep/xml?tag=stable64 -u https://npackd.appspot.com/rep/recent-xml?tag=untested -u https://npackd.appspot.com/rep/xml?tag=libs -u https://npackd.appspot.com/rep/xml?tag=unstable
 if %errorlevel% neq 0 exit /b %errorlevel%
@@ -20,7 +18,7 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 "%npackd_cl%\ncl" set-install-dir -f "C:\Program Files (x86)"
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-"%npackd_cl%\ncl" add -p com.googlecode.windows-package-manager.NpackdCL -p com.googlecode.windows-package-manager.NpackdInstallerHelper -p nircmd64 -v 2.75 -p com.googlecode.windows-package-manager.CLU -v 1.0.1 -p sysinternals-suite -p se.haxx.curl.CURL64
+"%npackd_cl%\ncl" add -p com.googlecode.windows-package-manager.NpackdCL -p com.googlecode.windows-package-manager.NpackdInstallerHelper -p com.googlecode.windows-package-manager.CLU -v 1.0.1 -p sysinternals-suite -p se.haxx.curl.CURL64
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 go get github.com/kbinani/screenshot
